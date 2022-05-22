e870040d68440a884bc08f0a6d9b67e649fa82ac
ElPioterro <101070139+ElPioterro@users.noreply.github.com>
2022-05-22 17:53:06 +0200

    Write in course id and cookies🍪.
    You can write cookie id and course id in cmd. Therefor you don't need to edit example.py file every time you want to download different skillshare course.

--- code/example.py
+++ code/example.py
@@ -1,13 +1,14 @@
 from downloader import Downloader
 
-cookie = """
-ADD YOUR COOKIE HERE
-"""
+cookie = "PHPSESSID=" + input("PHPSESSID=")
+#"PHPSESSID=779286ffc5a65d33c137acdeaaef7d41"
 
 dl = Downloader(cookie=cookie)
 
 # download by class URL:
-dl.download_course_by_url('https://www.skillshare.com/classes/Art-Fundamentals-in-One-Hour/189505397')
+#dl.download_course_by_url('https://www.skillshare.com/classes/Brand-Strategy-Build-a-Business-that-Lasts/2083092514')
 
 # or by class ID:
-# dl.download_course_by_class_id(189505397)
+#dl.download_course_by_class_id(2083092514)
+#while True:
+dl.download_course_by_class_id(input("Course id:"))
